0f96f247a9aeb36f9909f96a93e547e440fba3d0
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-22 21:18:16 +0200

    project

---
+++ StateWrapper/StateWrapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateWrapper
+//
+//  Created by Manyuchi, Carrington C on 2024/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateWrapper/StateWrapper/StateWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateWrapperApp.swift
+//  StateWrapper
+//
+//  Created by Manyuchi, Carrington C on 2024/08/22.
+//
+
+import SwiftUI
+
+@main
+struct StateWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
